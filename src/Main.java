6d98f4b27c023e78f2e609dced63daaf3782f120
Kristie Santa <k-santa@hotmail.com>
2025-07-05 14:59:56 -0400

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,44 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Input item price, calculate shipping cost, output shipping and total price
+
+        // Declare constants
+        final double FREE_SHIPPING_THRESHOLD = 100.0;
+        final double SHIPPING_RATE = 0.02;
+
+        // Declare variables
+        double itemPrice = 0.0;
+        double shippingCost = 0.0;
+        double totalPrice = 0.0;
+
+        // Create scanner for input
+        Scanner input = new Scanner(System.in);
+
+        // Get user input
+        System.out.print("Enter the price of the item: ");
+        itemPrice = input.nextDouble();
+
+        // Determine shipping cost
+        if (itemPrice >= FREE_SHIPPING_THRESHOLD) {
+            shippingCost = 0.0;
+        } else {
+            shippingCost = itemPrice * SHIPPING_RATE;
         }
+
+        // Calculate total price
+        totalPrice = itemPrice + shippingCost;
+
+        // Output results
+        System.out.println("Shipping cost: $" + shippingCost);
+        System.out.println("Total price including shipping: $" + totalPrice);
+
+        input.close();
     }
 }
